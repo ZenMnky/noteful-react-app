2f997b19d48ecb42f16e1bba65473c5f27aba4a4
Justin Hager <coachjustinhager@gmail.com>
2020-11-14 14:33:12 -0800

    adds all notes to Main view

--- src/Components/Main/MainMain.js
+++ src/Components/Main/MainMain.js
@@ -1,23 +1,25 @@
 import React, {Component} from 'react';
 import {Consumer} from '../Context'
+import moment from 'moment';
 
 class MainMain extends Component{
     render(){
         return(
             <Consumer>
                 {context => {
-                    let folders = context.folders.map(folder => {
+                    let notes = context.notes.map(note => {
                         return(
-                            <div>
-                                <p>{folder.name}</p>
-                                <p>{folder.id}</p>
+                            <div className="noteListItem" >
+                                <h2>{note.name}</h2>
+                                <p>Last modified: { moment(note.modified).calendar() }</p>
+                                <input type='button' value='Delete Note' />
                             </div>
                         );
                     })
                     return(
                         <div>
                             <h1>Main Main</h1>
-                            {folders}
+                            {notes}
                         </div>
                     );
                 }}
