3e775840b6af1a51457989f58cdbcf557f432cde
Justin Hager <coachjustinhager@gmail.com>
2020-11-17 10:43:54 -0800

    Add API fetch to /folders

--- src/Components/Context/index.js
+++ src/Components/Context/index.js
@@ -2,20 +2,56 @@ import React, {Component} from 'react';
 import DATA from '../../dummy-store';
 
 const NotefulContext = React.createContext();
+const API_ENDPOINT = 'http://localhost:9090';
 
 export class Provider extends Component {
     constructor(props){
         super(props);
         this.state={
           notes: [],
-          folders: []
+          folders: [],
+          error: null
         }
       }
 
+      addFolder = (folder) => {
+        this.setState({
+          folders: [...this.state.folders, folder]
+        })
+      }
+    /**
+     * implement two fetch requests: /folders and /notes endpoints
+     */
     componentDidMount = () => {
+      //fetch /folders
+      fetch(API_ENDPOINT + '/folders', {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+      .then(res => {
+        //if not okay, throw error
+        if(!res.ok){
+          throw new Error(res.status)
+        }
+        //other wise, return parsed response
+        console.log('response okay!')
+        return res.json();
+        
+      })
+      //update state
+      .then(folderData => this.setState({folders: folderData}))
+      //catch errors
+      .catch(error => this.setState({ error }))
+      
+
+      //fetch /notes
+      //parse to data
+      //update state
       this.setState({
           notes: DATA.notes,
-          folders: DATA.folders
+          
       })
     }
     
