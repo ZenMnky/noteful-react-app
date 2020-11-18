f6e0632264e9405dff3bee3b2b8db47ab0659a01
Justin Hager <coachjustinhager@gmail.com>
2020-11-18 06:03:11 -0800

    initialize ErrorBoundary module

---
+++ src/Components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+export default class ErrorBoundary extends Component{
+    state = {
+        hasError: false
+    };
+
+    componentDidCatch() {
+        this.setState({ hasError: true })
+    }
+    
+    render(){
+        
+        if (this.state.hasError){
+            return(
+                <h2>Hmmm...something isn't quite right here...</h2>
+            )
+        }
+        
+        return this.props.children
+    }
+}
